57b599d4b79f75c5d069b288a0df6d1b2cb2d206
Andrew Nguyen <ncuong.bkhn@gmail.com>
2017-07-11 22:47:14 +0700

    add transaction screen

--- design/js/app.js
+++ design/js/app.js
@@ -1,6 +1,11 @@
 $(document).foundation()
 
 $(document).ready(function(){
+	
+	var initUrl = $("#menu a").attr("data-href");
+	$.get( initUrl, function( data ) {
+	  $("#content").html(data);
+	});
 	$("#menu a").on('click', function(){
 		//url
 		//console.log($(this).attr("aria-selected"))
