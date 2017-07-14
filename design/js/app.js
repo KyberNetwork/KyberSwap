c0feba6dffc56f8204e0b75e01b54095a7f12247
Andrew Nguyen <ncuong.bkhn@gmail.com>
2017-07-14 15:27:21 +0700

    add exchange screen ver 1

--- design/js/app.js
+++ design/js/app.js
@@ -20,29 +20,30 @@ $(document).ready(function(){
 		//console.log(url);
 	})
 
+	 $('.selectric').selectric();
 	//for selected
-	$(".k-select").each(function(){
-		var _this = this;
-		if($(this).find('.k-select-option > div.selected').length !== 1){
-			(this).find('.k-select-option > div').removeClass('selected')
-			(this).find('.k-select-option > div:first-child').addClass('selected')
-		}
-		$(this).find(".k-select-display").html($(this).find('.k-select-option div.selected').html());
-		$(this).find(".k-select-display").on('click', function(){
-			var option = $(_this).find('.k-select-option');
-			if(option.is(":visible")){
-				$(_this).find('.k-select-option').hide();	
-			}else{
-				$(_this).find('.k-select-option').show();	
-			}			
-		})
-		$(this).find(".k-select-option > div").on('click', function(){
-			if(!$(this).hasClass('selected')){
-				$(_this).find('.k-select-option >div.selected').removeClass('selected');
-				$(this).addClass('selected')
-				$(_this).find(".k-select-display").html($(this).html());				
-			}
-			$(_this).find('.k-select-option').hide();
-		})
-	})
+	// $(".k-select").each(function(){
+	// 	var _this = this;
+	// 	if($(this).find('.k-select-option > div.selected').length !== 1){
+	// 		(this).find('.k-select-option > div').removeClass('selected')
+	// 		(this).find('.k-select-option > div:first-child').addClass('selected')
+	// 	}
+	// 	$(this).find(".k-select-display").html($(this).find('.k-select-option div.selected').html());
+	// 	$(this).find(".k-select-display").on('click', function(){
+	// 		var option = $(_this).find('.k-select-option');
+	// 		if(option.is(":visible")){
+	// 			$(_this).find('.k-select-option').hide();	
+	// 		}else{
+	// 			$(_this).find('.k-select-option').show();	
+	// 		}			
+	// 	})
+	// 	$(this).find(".k-select-option > div").on('click', function(){
+	// 		if(!$(this).hasClass('selected')){
+	// 			$(_this).find('.k-select-option >div.selected').removeClass('selected');
+	// 			$(this).addClass('selected')
+	// 			$(_this).find(".k-select-display").html($(this).html());				
+	// 		}
+	// 		$(_this).find('.k-select-option').hide();
+	// 	})
+	// })
 })
