d1349a37931c29f1ba0e0c4ca6870629a796d174
tranvictor <vu.tran54@gmail.com>
2017-07-28 11:54:59 +0700

    update token logos

--- src/js/services/supported_tokens.js
+++ src/js/services/supported_tokens.js
@@ -2,19 +2,19 @@ var TOKENS = [
   {
     name: "Golem",
     symbol: "GNT",
-    icon: "https://etherscan.io/token/images/digix-logo.png",
+    icon: "/assets/golem-logo.png",
     address: "0xbb9c28a16654a0cd510f5b0a250255c4a07211f2"
   },
   {
     name: "Digix",
     symbol: "DGD",
-    icon: "https://etherscan.io/token/images/melon.jpg",
+    icon: "/assets/digix-logo.png",
     address: "0x950b87923d52b09b1050abda589f91521e17e606"
   },
   {
     name: "Gnosis",
     symbol: "GNO",
-    icon: "https://etherscan.io/token/images/smartpool.png",
+    icon: "/assets/gnosis-logo.png",
     address: "0x2c018fc6c9bb2b7653136dc7c5b7b588f2d11986"
   },
 ]
