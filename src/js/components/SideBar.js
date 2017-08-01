5a56168012843f5f3fb5556f9b9c5ed74d2119bf
Andrew Nguyen <ncuong.bkhn@gmail.com>
2017-08-01 12:40:39 +0700

    add animation

--- src/js/components/SideBar.js
+++ src/js/components/SideBar.js
@@ -58,18 +58,10 @@ export default class SideBar extends React.Component {
           </div>
           <div>Transactions</div>
           <div class="loading-icon">
-            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" xmlSpace="preserve">
-              <rect x="0" y="0" width="4" height="7" fill="#333" transform="scale(1 2.16074)">
-                <animateTransform attributeType="xml" attributeName="transform" type="scale" values="1,1; 1,3; 1,1" begin="0s" dur="0.6s" repeatCount="indefinite"></animateTransform>       
-              </rect>
-
-              <rect x="10" y="0" width="4" height="7" fill="#333" transform="scale(1 1.17259)">
-                <animateTransform attributeType="xml" attributeName="transform" type="scale" values="1,1; 1,3; 1,1" begin="0.2s" dur="0.6s" repeatCount="indefinite"></animateTransform>       
-              </rect>
-              <rect x="20" y="0" width="4" height="7" fill="#333" transform="scale(1 2.50592)">
-                <animateTransform attributeType="xml" attributeName="transform" type="scale" values="1,1; 1,3; 1,1" begin="0.4s" dur="0.6s" repeatCount="indefinite"></animateTransform>       
-              </rect>
-            </svg>
+            <div class='bullet'></div>
+  <div class='bullet'></div>
+  <div class='bullet'></div>
+  <div class='bullet'></div>
           </div>
         </div>
       </div>
