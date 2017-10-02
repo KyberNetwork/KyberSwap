cb1019cf59e516b53fd86254862f3596d086e2e6
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2017-10-02 11:47:58 +0700

    config load scss from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   context: path.join(__dirname, "src"),
   devtool: debug ? "inline-sourcemap" : false,
-  entry: "./js/client.js",
+  entry: ["./js/client.js", "./assets/scss/app.scss"],
   module: {
-    loaders: [
-      {
+    loaders: [{
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
@@ -16,6 +16,38 @@ module.exports = {
           presets: ['react', 'es2015', 'stage-0'],
           plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
         }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [{
+            loader: 'file-loader',
+            options: {
+              query: {
+                name: '[name].[ext]'
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              query: {
+                mozjpeg: {
+                  progressive: true,
+                },
+                gifsicle: {
+                  interlaced: true,
+                },
+                optipng: {
+                  optimizationLevel: 7,
+                }
+              }
+            }
+          }
+        ]
       }
     ]
   },
@@ -23,10 +55,19 @@ module.exports = {
     path: __dirname + "/src/",
     filename: "client.min.js"
   },
-  plugins: debug ? [] : [
+  plugins: debug ? [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'app.bundle.css',
+      allChunks: true,
+    })
+  ] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'app.bundle.css',
+      allChunks: true,
+    })
   ],
   devServer: {
     compress: true,
