0619a492fa8b50496b3ff9870bf929ee559422ac
tranvictor <vu.tran54@gmail.com>
2017-10-13 00:19:51 +0700

    update minor

--- src/js/services/supported_tokens.js
+++ src/js/services/supported_tokens.js
@@ -11,12 +11,12 @@ var TOKENS = [
     icon: "/assets/digix-logo.png",
     address: "0x950b87923d52b09b1050abda589f91521e17e606"
   },
-  // {
-  //   name: "Gnosis",
-  //   symbol: "GNO",
-  //   icon: "/assets/gnosis-logo.png",
-  //   address: "0x2c018fc6c9bb2b7653136dc7c5b7b588f2d11986"
-  // },
+  {
+    name: "Gnosis",
+    symbol: "GNO",
+    icon: "/assets/gnosis-logo.png",
+    address: "0x2c018fc6c9bb2b7653136dc7c5b7b588f2d11986"
+  },
 ]
 
 export default TOKENS
