56537098de53066bc72baa3137357bf4708045aa
Tuan Nguyen <mtuan.94@outlook.com>
2017-10-16 16:29:46 +0700

    adding rate table, update interval 10s

--- src/js/components/ExchangeRates/ExchangeRates.js
+++ src/js/components/ExchangeRates/ExchangeRates.js
@@ -5,59 +5,36 @@ import SupportedTokens from "../../services/supported_tokens"
 import constants from "../../services/constants"
 
 const ExchangeRates = (props) => {
-	var tokens = [{
+	var ether = {
       name: "Ether",
       symbol: "ETH",
       icon: "/img/ether.png",
-      address: constants.ETHER_ADDRESS}]
-    for (var i = 0; i < SupportedTokens.length; i++) {
-      tokens.push({
-        name: SupportedTokens[i].name,
-        icon: SupportedTokens[i].icon,
-        symbol: SupportedTokens[i].symbol,
-        address: SupportedTokens[i].address
-      })
+      address: constants.ETHER_ADDRESS
+  };
+  var rates = []
+  var rateID
+  var rate
+  var rateReverse
+  for (var i = 0; i < SupportedTokens.length; i++) {
+    var token = {
+      name: SupportedTokens[i].name,
+      icon: SupportedTokens[i].icon,
+      symbol: SupportedTokens[i].symbol,
+      address: SupportedTokens[i].address
+    };
+    rateID = token.address + "-" + ether.address
+    rate = props.rates[rateID]
+    if (rate) {
+      rates.push(
+        <tr key={rateID}>
+          <td class="token-pair">{rate.source.symbol}</td>
+          <td title={toT(rate.rate)}>{toT(rate.rate, 8)}</td>
+        </tr>
+      )
     }
-    var rates = []
-    var rateID
-    var rate
-    var rateReverse
-    for (var i = 0; i < tokens.length; i++) {
-      for (var j = i + 1; j < tokens.length; j++) {
-        if (i == 0 || j == 0) {
-          rateID = tokens[i].address + "-" + tokens[j].address
-          rate = props.rates[rateID]
-          rateID = tokens[j].address + "-" + tokens[i].address
-          rateReverse = props.rates[rateID]
-          if (rate && rateReverse) {
-            rates.push(
-              <tr key={rateID}>
-                <td class="token-pair">{rate.source.symbol}-{rate.dest.symbol}</td>
-                <td title={toT(rate.rate)}>{toT(rate.rate, 8)}</td>
-                <td class="token-pair">{rateReverse.source.symbol}-{rateReverse.dest.symbol}</td>
-                <td title={toT(rateReverse.rate)}>{toT(rateReverse.rate, 8)}</td>
-              </tr>
-            )
-          }
-        }
-      }
-    }
+  }
 	return (
     <table>
-      <thead>
-        <tr>
-          <th width="100"></th>
-          <th width="150">
-            <i class="k-icon k-icon-rate"></i>
-            Rate
-          </th>
-          <th width="100"></th>
-          <th width="150">
-            <i class="k-icon k-icon-rate"></i>
-            Rate
-          </th>
-        </tr>
-      </thead>
       <tbody>
         {rates.length > 0 ?
           rates :
--- src/js/containers/Header/Rate.js
+++ src/js/containers/Header/Rate.js
@@ -1,12 +1,19 @@
 import React from "react"
 import { connect } from "react-redux"
+import { ExchangeRates } from "../../components/ExchangeRates"
 
+@connect((store) => {
+  return {
+    rates: store.global.rates
+  }
+})
 
 export default class Rate extends React.Component {
 	render() {
     return (
         <div>
           This is rate table
+          <ExchangeRates rates = {this.props.rates}/>
         </div>
     )
   }
