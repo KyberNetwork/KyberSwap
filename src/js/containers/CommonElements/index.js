3063460f5b4755aba9d37c05d8e085280eacad85
Tuan Nguyen <mtuan.94@outlook.com>
2017-10-21 15:24:59 +0700

    reset passphraseModal

--- src/js/containers/CommonElements/index.js
+++ src/js/containers/CommonElements/index.js
@@ -8,6 +8,6 @@ export { default as ConfirmModal } from './ConfirmModal.js';
 
 export { default as SelectTokenModal } from './SelectTokenModal.js';
 export { default as ChangeGasModal } from './ChangeGasModal.js';
-// export { default as PassphraseModal } from './PassphraseModal.js';
+export { default as PassphraseModal } from './PassphraseModal.js';
 export { default as PassphraseTransferModal } from './PassphraseTransferModal.js';
 export { default as PassphraseExchangeModal } from './PassphraseExchangeModal.js';
