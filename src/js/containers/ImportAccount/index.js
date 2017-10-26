d1797f4f6c9286f8cf5c5968ed15da5d8007af2e
Tran Bao Huy <huy23121994@gmail.com>
2017-10-26 18:35:36 +0700

    update import account screen

--- src/js/containers/ImportAccount/index.js
+++ src/js/containers/ImportAccount/index.js
@@ -1,4 +1,4 @@
 
 export { default as ImportKeystore } from './ImportKeystore.js';
-export { default as ImportTrezor } from './ImportByDevice.js';
+export { default as ImportByDevice } from './ImportByDevice.js';
 export { default as ImportAccount } from './ImportAccount.js';;
