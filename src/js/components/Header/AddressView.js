85f70905e862acc3fd7e6675f80f4fb45bd92ac2
Tuan Nguyen <mtuan.94@outlook.com>
2017-10-26 09:58:29 +0700

    fix header

--- src/js/components/Header/AddressView.js
+++ src/js/components/Header/AddressView.js
@@ -4,55 +4,33 @@ import React from "react"
 
 const Address = (props) => {
 
-  const newSession = (
-    <div class="user-bar text-right"><a class="button accent" href="/import-address.html">Import Address</a></div>  
-  )
-
-  const loged = (
-    [
-      <div key="1" class="column small-3">
-        <a class="avatar" href="/exchange.html">
-          <img src="/assets/img/address.png"/>
-        </a>
-      </div>,
-      <div key="2" class="column small-9">
-        {<a class="short-address" href="/exchange.html">0xd1498f ... 7d0909</a>}
-        {/* {props.address.slice(0,8)} ... {props.address.slice(-6)} */}
-        <ul class="actions">
-          <li><Link to="/transfer" >
-                Transfer
-              </Link>
-          </li>
-          <li><a onClick={props.endSession} class="exit">
-                End Session
-              </a>
-          </li>
-        </ul>
+  return props.address ?
+    (
+      <div class="column small-10">
+        <div key="1" class="column small-3">
+          <a class="avatar" href="/exchange.html">
+            <img src="/assets/img/address.png"/>
+          </a>
+        </div>
+        <div key="2" class="column small-9">
+          <a class="short-address" href="/exchange.html">{props.address ? props.address.slice(0,8):''} ... {props.address? props.address.slice(-6):''}</a>
+          <ul class="actions">
+            <li><Link to="/transfer" >
+                  Transfer
+                </Link>
+            </li>
+            <li><a onClick={props.endSession} class="exit">
+                  End Session
+                </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    ]
-  )
-
-  // return (true ? loged : newSession)
-
-  return (
-    <div class="column small-10">
-        { true ? loged: newSession}
-                
-        
-
-
-      {/* <p>{ props.address ? props.address : ''}</p>
-      <Link to="/transfer" >
-        Transfer
-      </Link>
-      <br/>
-      <Link to="/exchange" >
-        ExChange
-      </Link>
-      <br/>
-      <a onClick={props.endSession}>End session</a> */}
-    </div>
-  )
+    ) : (
+      <div class="column small-10">
+        <div class="user-bar text-right"><a class="button accent" href="/import-address.html">Import Address</a></div>  
+      </div>
+    )
 }
 
 export default Address
