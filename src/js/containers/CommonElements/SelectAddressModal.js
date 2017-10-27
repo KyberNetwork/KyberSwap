42b607dafb8f742165ddff3f90338d7ea095bbdd
Tran Bao Huy <huy23121994@gmail.com>
2017-10-27 16:38:44 +0700

    custom path to  get address

--- src/js/containers/CommonElements/SelectAddressModal.js
+++ src/js/containers/CommonElements/SelectAddressModal.js
@@ -6,12 +6,31 @@ import { Modal } from '../../components/CommonElement'
 export default class SelectAddressModal extends React.Component {
 
     render() {
+
+        const customStyles = {
+            addresses: {
+                overflowY: 'auto',
+                height: 700
+            },
+        }
         return (
+
             <Modal
+                className={{ base: 'reveal large', afterOpen: 'reveal large' }}
                 isOpen={this.props.open}
                 onRequestClose={this.props.onRequestClose}
                 contentLabel="Select Address"
-                content={this.props.content}
+                content={
+                    <div style={customStyles.addresses}>
+                        <div class="title">Select Address</div><a class="x" onClick={this.props.onRequestClose}>&times;</a>
+                        <div class="content">
+                            <div class="row tokens small-up-2 medium-up-3 large-up-4">
+                                {this.props.content}
+                            </div>
+                        </div>
+                    </div>
+
+                }
             />
 
         )
--- src/js/containers/ImportAccount/ImportByDevice.js
+++ src/js/containers/ImportAccount/ImportByDevice.js
@@ -7,197 +7,291 @@ import { getTrezorPublicKey, connectLedger, getLedgerPublicKey } from "../../ser
 import SelectAddressModal from "../CommonElements/SelectAddressModal";
 
 import { importNewAccount } from "../../actions/accountActions"
+import { toEther } from "../../utils/converter"
+
 @connect((store) => {
-    return { ...store.account }
+	return {
+		ethereumNode: store.connection.ethereum,
+		account: store.account,
+	}
 })
+
 
 export default class ImportByDevice extends React.Component {
 
-    constructor() {
-        super();
-        this.state = {
-            addresses: [],
-            currentAddresses: [],
-            modalOpen: false,
-        }
-        this.setDeviceState();
-    }
+	constructor() {
+		super();
+		this.state = {
+			addresses: [],
+			currentAddresses: [],
+			modalOpen: false,
+		}
+		this.setDeviceState();
 
-    setDeviceState() {
-        this.addressIndex = 0;
-        this.currentIndex = 0;
-        this.walletType = 'trezor';
-        this.dPath = '';
-        this.generator = null;
-    }
+		this.DPATH = [
+			{ path: "m/44'/60'/0'/0", desc: 'Jaxx, Metamask, Exodus, imToken, TREZOR (ETH) & Digital Bitbox', defaultType: 'trezor' },
+			{ path: "m/44'/60'/0'", desc: 'Ledger (ETH)', defaultType: 'ledger' },
+			{ path: "m/44'/61'/0'/0", desc: 'TREZOR (ETC)' },
+			{ path: "m/44'/60'/160720'/0'", desc: 'Ledger (ETC)' },
+			{ path: 0, desc: 'Your Custom Path', defaultP: "m/44'/60'/1'/0" },
+			{ path: "m/44'/1'/0'/0", desc: 'Network: Testnets' },
+			{ path: "m/44'/40'/0'/0", desc: 'Network: Expanse' },
+		]
+	}
 
-    connectDevice(walletType) {
-        this.setDeviceState();
-        switch (walletType) {
-            case 'trezor': {
-                let promise = getTrezorPublicKey();
-                promise.then((result) => {
-                    this.generateAddress(result);
-                    this.dPath = result.dPath;
-                })
-                break;
-            }
-            case 'ledger': {
-                connectLedger().then((eth) => {
-                    getLedgerPublicKey(eth).then((result) => {
-                        this.generateAddress(result);
-                        this.dPath = result.dPath;
-                    }).catch((err) => {
-                        console.log(err)
-                    });
-                }).catch((err) => {
-                    console.log(err)
-                });
-                break;
-            }
-        }
-        console.log(this.dPath);
-        this.walletType = walletType;
-    }
+	setDeviceState() {
+		this.addressIndex = 0;
+		this.currentIndex = 0;
+		this.walletType = 'trezor';
+		this.dPath = '';
+		this.generator = null;
+	}
 
-    generateAddress(data) {
-        this.generator = new AddressGenerator(data);
-        let addresses = [];
-        let index = 0;
-        for (index; index < 5; index++) {
-            let address = {
-                addressString: this.generator.getAddressString(index),
-                index: index,
-            };
-            addresses.push(address);
-        }
-        this.addressIndex = index;
-        this.currentIndex += 5;
+	connectDevice(walletType, path) {
+		this.setDeviceState();
+		switch (walletType) {
+			case 'trezor': {
+				let promise = getTrezorPublicKey(path);
+				promise.then((result) => {
+					this.generateAddress(result);
+					this.dPath = result.dPath;
+				})
+				break;
+			}
+			case 'ledger': {
 
-        this.setState({
-            addresses: addresses,
-            currentAddresses: addresses.slice(this.currentIndex - 5, this.currentIndex)
-        })
-        this.openModal();
-    }
+				connectLedger().then((eth) => {
+					getLedgerPublicKey(eth, path).then((result) => {
+						this.generateAddress(result);
+						this.dPath = result.dPath;
+					}).catch((err) => {
+						console.log(err)
+					});
+				}).catch((err) => {
+					console.log(err)
+				});
+				break;
+			}
+		}
+		this.walletType = walletType;
+	}
 
-    openModal() {
-        this.setState({
-            modalOpen: true,
-        })
-    }
+	generateAddress(data) {
+		this.generator = new AddressGenerator(data);
+		let addresses = [];
+		let index = 0;
+		for (index; index < 5; index++) {
+			let address = {
+				addressString: this.generator.getAddressString(index),
+				index: index,
+				balance: 'loading...',
+			};
+			addresses.push(address);
+			this.updateBalance(address.addressString, index);
+		}
+		this.addressIndex = index;
+		this.currentIndex = index;
 
-    closeModal() {
-        this.setState({
-            modalOpen: false,
-        })
-    }
+		this.setState({
+			addresses: addresses,
+			currentAddresses: addresses
+		})
+		if (!this.state.modalOpen) this.openModal();
+	}
 
-    moreAddress() {
-        let addresses = this.state.addresses,
-            i = this.addressIndex,
-            j = i + 5;
-        if (this.addressIndex == this.currentIndex) {
-            for (i; i < j; i++) {
-                let address = {
-                    addressString: this.generator.getAddressString(i),
-                    index: i,
-                };
-                addresses.push(address);
-            }
-        }
-        this.addressIndex = i;
-        this.currentIndex += 5;
+	openModal() {
+		this.setState({
+			modalOpen: true,
+		})
+	}
 
-        this.setState({
-            addresses: addresses,
-            currentAddresses: addresses.slice(this.currentIndex - 5, this.currentIndex)
-        })
-    }
+	closeModal() {
+		this.setState({
+			modalOpen: false,
+		})
+	}
 
-    preAddress() {
-        let addresses = this.state.addresses;
-        if (this.currentIndex > 5) {
-            this.currentIndex -= 5;
-            this.setState({
-                currentAddresses: addresses.slice(this.currentIndex - 5, this.currentIndex)
-            })
-        }
-    }
+	moreAddress() {
+		let addresses = this.state.addresses,
+			i = this.addressIndex,
+			j = i + 5,
+			currentAddresses = [];
+		console.log('before ', this.currentIndex, this.addressIndex);
+		console.log('before ', this.state);
+		if (this.addressIndex == this.currentIndex) {
+			for (i; i < j; i++) {
+				let address = {
+					addressString: this.generator.getAddressString(i),
+					index: i,
+					balance: 'loading...',
+				};
+				addresses.push(address);
+				currentAddresses.push(address);
+				this.updateBalance(address.addressString, i);
+			
+			}
+		}
+		this.addressIndex = i;
+		this.currentIndex += 5;
+		console.log('after ', this.currentIndex, this.addressIndex);
+		console.log('after ', this.state);
 
-    getAddress() {
-        let formAddress = JSON.parse(this.refs.formAddress.address.value),
-            data = {
-                address: formAddress.addressString,
-                type: this.walletType,
-                path: this.dPath + '/' + formAddress.index,
+		this.setState({
+			addresses: addresses,
+			currentAddresses: addresses.slice(this.currentIndex - 5, this.currentIndex)
+		})
+	}
 
-            };
+	preAddress() {
+		let addresses = this.state.addresses;
+		if (this.currentIndex > 5) {
+			this.currentIndex -= 5;
+			this.setState({
+				currentAddresses: addresses.slice(this.currentIndex - 5, this.currentIndex)
+			})
+		}
+	}
 
-        this.props.dispatch(importNewAccount(data.address, data.type, data.path))
-        this.closeModal()
-        // setTimeout(() => { this.goToExchange() }, 3000)
-    }
-    goToExchange = () => {
-        // window.location.href = "/exchange"
-        // this.props.router.push('/exchange')
-        this.props.dispatch(push('/exchange'));
-    }
-    render() {
-        const currentList = this.state.currentAddresses.map((address, index) => {
-            return (
-                <li key={address.addressString}>
-                    <label>
-                        <input type="radio"
-                            name="address"
-                            value={JSON.stringify(address)}
-                            defaultChecked={index == 0 ? true : false} />
-                        {address.addressString}
-                    </label>
-                </li>
-            )
-        })
+	getAddress() {
+		let formAddress = JSON.parse(this.refs.formAddress.address.value),
+			data = {
+				address: formAddress.addressString,
+				type: this.walletType,
+				path: this.dPath + '/' + formAddress.index,
 
-        return (
-            <div>
-                <div class="small-12 medium-6 column" style={{padding: 0}}>
-                    <div class="column column-block">
-                        <div class="importer trezor">
-                            <a href="#" onClick={this.connectDevice.bind(this, 'trezor')}>
-                                <img src="/assets/img/trezor.svg" />
-                                <div class="description">Import from<br />trezor</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="small-12 medium-6 column" style={{padding: 0}}>
-                    <div class="column column-block">
-                        <div class="importer ledger">
-                            <a href="#" onClick={this.connectDevice.bind(this, 'ledger')}>
-                                <img src="/assets/img/ledger.svg" />
-                                <div class="description">Import from<br />ledger wallet</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <SelectAddressModal
-                    open={this.state.modalOpen}
-                    onRequestClose={this.closeModal.bind(this)}
-                    content={
-                        <div className="popup">
-                            <form ref="formAddress">
-                                <ul>
-                                    {currentList}
-                                </ul>
-                            </form>
-                            <a onClick={() => this.preAddress()}>Pre address</a> |
-                            <a onClick={() => this.moreAddress()}> More address</a> |
-                            <a onClick={() => this.getAddress()}> Select address</a>
-                        </div>
-                    }
-                />
+			};
 
-            </div>
-        )
-    }
+		this.props.dispatch(importNewAccount(data.address, data.type, data.path))
+		this.closeModal()
+		// setTimeout(() => { this.goToExchange() }, 3000)
+	}
+	goToExchange = () => {
+		this.props.dispatch(push('/exchange'));
+	}
+
+	choosePath = () => {
+		let formPath = this.refs.formPath,
+			path = formPath.path.value;
+
+		path = (path != 0) ? path : formPath.customPath.value;
+		this.connectDevice(this.walletType, path);
+	}
+
+	getBalance(address) {
+		return new Promise((resolve, reject) => {
+			this.props.ethereumNode.getBalance(address, (balance) => {
+				resolve(toEther(balance))
+			})
+		})
+	}
+
+	getCurrentList() {
+		let currentListHtml = this.state.currentAddresses.map((address, index) => {
+			return (
+				<li key={address.addressString}>
+					<label style={{ marginBottom: 0 }}>
+						<input type="radio"
+							name="address"
+							value={JSON.stringify(address)}
+							defaultChecked={index == 0 ? true : false}
+						/>
+						{address.addressString} - <span> {address.balance} ETH</span>
+						<br />
+					</label>
+				</li>
+			)
+		})
+		return currentListHtml;
+	}
+
+	updateBalance(address, index) {
+		this.getBalance(address)
+			.then((result) => {
+				let addresses = this.state.addresses;
+				addresses[index].balance = result
+				this.setState({
+					currentList: addresses
+				})
+				
+			})
+	}
+
+	render() {
+		let listPath = this.DPATH.map((dPath, index) => {
+			return (
+				<label style={{ width: 200, float: 'left' }} key={dPath.path}>
+					<input type="radio" name="path"
+						defaultValue={dPath.path}
+						defaultChecked={(dPath.defaultType == this.walletType) ? true : false}
+					/>
+					{
+						dPath.path ? 
+						(
+							<span>
+								{dPath.path}
+								<br />
+								{dPath.desc}
+							</span>
+						) : (
+							<span>
+								{dPath.desc}
+								<input type="text" name="customPath"
+									defaultValue={dPath.defaultP}
+								/>
+							</span>
+						)
+					}
+				</label>
+			)
+		})
+		return (
+			<div>
+				<div class="small-12 medium-6 column" style={{ padding: 0 }}>
+					<div class="column column-block">
+						<div class="importer trezor">
+							<a href="#" onClick={() => this.connectDevice('trezor')}>
+								<img src="/assets/img/trezor.svg" />
+								<div class="description">Import from<br />trezor</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="small-12 medium-6 column" style={{ padding: 0 }}>
+					<div class="column column-block">
+						<div class="importer ledger">
+							<a href="#" onClick={() => this.connectDevice('ledger')}>
+								<img src="/assets/img/ledger.svg" />
+								<div class="description">Import from<br />ledger wallet</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<SelectAddressModal
+					open={this.state.modalOpen}
+					onRequestClose={this.closeModal.bind(this)}
+					content={
+						<div className="popup">
+							<form ref="formPath">
+								{listPath}
+								<div style={{ clear: 'both' }}></div>
+							</form>
+
+							<button class="button expand" onClick={() => this.choosePath()}>Change path</button>
+							<br/>
+
+							<form ref="formAddress">
+								<ul>
+									{this.getCurrentList()}
+								</ul>
+							</form>
+							<a onClick={() => this.preAddress()}>Pre address</a> |
+							<a onClick={() => this.moreAddress()}> More address</a> |
+							<a onClick={() => this.getAddress()}> Select address</a>
+						</div>
+					}
+				/>
+
+			</div>
+		)
+	}
 }
