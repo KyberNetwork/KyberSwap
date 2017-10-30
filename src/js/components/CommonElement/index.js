fea1b254de24db69c9d39a95d925bd74f1c6b113
Tuan Nguyen <mtuan.94@outlook.com>
2017-10-30 10:48:28 +0700

    add processing modal

--- src/js/components/CommonElement/index.js
+++ src/js/components/CommonElement/index.js
@@ -4,3 +4,4 @@ export { default as ReCredential } from './ReCredential.js';
 export { default as DropFile } from './DropFile.js';
 export { default as Credential } from './Credential.js';
 export { default as TransactionConfig } from './TransactionConfig.js';
+export { default as ProcessingModal } from "./Processing.js"
---
+++ src/js/containers/CommonElements/Processing.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { connect } from "react-redux"
+import { push } from 'react-router-redux';
+import { ProcessingModal } from '../../components/CommonElement'
+
+@connect((store) => {
+  return {account: store.account}
+})
+
+export default class Processing extends React.Component {
+  
+
+  render(){
+    var processingModal = (
+      <ProcessingModal />
+    )
+    return (
+      <ProcessingModal />
+    )  
+  }
+}
+
+///  {this.props.account && this.props.account.loading ? processingModal : 'a'}
