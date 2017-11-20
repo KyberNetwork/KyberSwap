40067cc04ff462b1d64c9bdff8ad3e67c4966447
Tran Bao Huy <huy23121994@gmail.com>
2017-11-20 09:45:15 +0700

    commit

--- src/js/components/Transaction/ExchangeForm.js
+++ src/js/components/Transaction/ExchangeForm.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {roundingNumber} from "../../utils/converter"
 
 const ExchangeForm = (props) => {
   var errorToken = props.errors.selectSameToken + props.errors.selectTokenToken
