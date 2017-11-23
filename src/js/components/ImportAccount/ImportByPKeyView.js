c9bf833e3d3dfb302af4e6cc03a0ed1a3c3724be
Tran Bao Huy <huy23121994@gmail.com>
2017-11-23 11:41:09 +0700

    toggle show passphare

--- src/js/components/ImportAccount/ImportByPKeyView.js
+++ src/js/components/ImportAccount/ImportByPKeyView.js
@@ -8,12 +8,23 @@ const ImportByPKeyView = (props) => {
 		props.importPrivateKey()
 	}
 
+	function toggleShowPw(){
+		let input = document.getElementById('private_key')
+		if(input.type == 'password'){
+			input.type = 'text'
+			input.parentElement.classList.add('unlock')
+		}else if(input.type == 'text'){
+			input.type = 'password'
+			input.parentElement.classList.remove('unlock')
+		}
+	}
+
 	return (
 		<div class="column column-block">
 			<div class="importer pkey">
 				<a onClick={() => props.modalOpen()}>
-						<img src="/assets/img/pkey.svg" />
-						<div class="description">Enter your<br />private key</div>
+					<img src="/assets/img/pkey.svg" />
+					<div class="description">Enter your<br />private key</div>
 				</a>
 			</div>
 
@@ -24,27 +35,29 @@ const ImportByPKeyView = (props) => {
 				content={
 					<div>
 						<div class="title">ENTER YOUR PRIVATE KEY</div><a class="x" onClick={props.onRequestClose}>&times;</a>
-						{/* <form onSubmit={(e) => handldeSubmit(e)}></form> */}
 						<div class="content with-overlap">
 							<div class="row">
 								<div class="column">
-									<center>
-										<label className={!!props.pKeyError ? "error" : ""}>
-											<input class="text-center" type="password" id="private_key" onChange={(e) => props.onChange(e)}
-											 placeholder="Enter your private key" required autoComplete="off" />
-											{!!props.pKeyError &&
-													<span className="error-text">{props.pKeyError}</span>
-											}
-										</label>
+									<center style={{marginBottom: 25}} className={!!props.pKeyError ? "error" : ""}>
+										<div className={'input-reveal'}>
+											<input class="text-center" id="private_key"
+												type="password"
+												onChange={(e) => props.onChange(e)}
+												placeholder="Enter your private key" required />
+											<a class="toggle" onClick={() => toggleShowPw()}></a>
+										</div>
+										{!!props.pKeyError &&
+											<span className="error-text">{props.pKeyError}</span>
+										}
 									</center>
 								</div>
 							</div>
 						</div>
 						<span className="error-text">kjsjdhfkjsdfk</span>
 						<div class="overlap">
-								<button class="button accent" onClick={(e) => handldeSubmit(e)} >Import</button>
+							<button class="button accent" onClick={(e) => handldeSubmit(e)} >Import</button>
 						</div>
-							
+
 					</div>
 				}
 			/>
