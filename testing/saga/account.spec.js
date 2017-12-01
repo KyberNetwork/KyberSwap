ebebc9caeaa20cd72640c2a32415a8d50d78c452
Tuan Nguyen <mtuan.94@outlook.com>
2017-12-01 19:28:03 +0700

    fix update account test

--- testing/saga/account.spec.js
+++ testing/saga/account.spec.js
@@ -84,17 +84,17 @@ const account = new Account(
 
 it('handle update account pending', () => {
   let fakeAccount = new Account(
-  "0xf34791ada19af51d5b0dc927b8420a2c7dc9b704", 
+  "0xd42a313bbe64c2525ff70d449f6bb589b6c6c6f5", 
   "keystore",
-  '{"version":3,"id":"04933224-8510-45f3-a3e2-882a8c149081","address":"f34791ada19af51d5b0dc927b8420a2c7dc9b704","crypto":{"ciphertext":"e4c51957a31da57e754498fd11da81d50424f3c2647fda816bc08ca51734f780","cipherparams":{"iv":"9f409c69b2c1eaf26c41e0ac8afd63cc"},"cipher":"aes-128-ctr","kdf":"pbkdf2","kdfparams":{"dklen":32,"salt":"07e47b0e900abe1aa4e2125e746a11ba7f6e87a696626c6ed0715113e3e4e75a","c":10240,"prf":"hmac-sha256"},"mac":"51d2dcb0a863a590d1c94bdbe315f0d9ec2ab3aa93f653d77922588a96ee2a65"}}'
+  '{"version":3,"id":"05272513-7306-4af2-882e-cee79a397c95","address":"d42a313bbe64c2525ff70d449f6bb589b6c6c6f5","crypto":{"ciphertext":"29c43c83b60a4fe76b26d2a724a440210037164136f03b9f5d67a4527d93b52a","cipherparams":{"iv":"506535b8298c83653a99e8326e2848e5"},"cipher":"aes-128-ctr","kdf":"pbkdf2","kdfparams":{"dklen":32,"salt":"03274eacc0f28b7bcaf5086707e9792b40573857e4000d013c70a1c8ca53b911","c":10240,"prf":"hmac-sha256"},"mac":"1d915b45d2d78197df3ba136e4b8b551356d672942b99dd5b6aa71e2b01a1c26"}}'
   );
-  fakeAccount.balance = '312021173902076846';
-  fakeAccount.nonce = 22;
-  fakeAccount.manualNonce = 22;
+  fakeAccount.balance = '1000000000000000000';
+  fakeAccount.nonce = 0;
+  fakeAccount.manualNonce = 0;
 
-  return expectSaga(updateAccount, {payload : { account: account, ethereum: ethereum}})
+  return expectSaga(updateAccount, {payload : { account: fakeAccount, ethereum: ethereum}})
     .provide([
-      [call(account.sync, fakeAccount ), ethereum, account]
+      [call(fakeAccount.sync, fakeAccount ), ethereum, fakeAccount]
     ])
     .put({
       type: 'ACCOUNT.UPDATE_ACCOUNT_FULFILLED',
@@ -106,13 +106,13 @@ it('handle update account pending', () => {
 
 it('import new account pending', () => {
   let fakeAccount = new Account(
-    "0xf34791ada19af51d5b0dc927b8420a2c7dc9b704", 
+    "0xd42a313bbe64c2525ff70d449f6bb589b6c6c6f5", 
     "keystore",
-    '{"version":3,"id":"04933224-8510-45f3-a3e2-882a8c149081","address":"f34791ada19af51d5b0dc927b8420a2c7dc9b704","crypto":{"ciphertext":"e4c51957a31da57e754498fd11da81d50424f3c2647fda816bc08ca51734f780","cipherparams":{"iv":"9f409c69b2c1eaf26c41e0ac8afd63cc"},"cipher":"aes-128-ctr","kdf":"pbkdf2","kdfparams":{"dklen":32,"salt":"07e47b0e900abe1aa4e2125e746a11ba7f6e87a696626c6ed0715113e3e4e75a","c":10240,"prf":"hmac-sha256"},"mac":"51d2dcb0a863a590d1c94bdbe315f0d9ec2ab3aa93f653d77922588a96ee2a65"}}'
+    '{"version":3,"id":"05272513-7306-4af2-882e-cee79a397c95","address":"d42a313bbe64c2525ff70d449f6bb589b6c6c6f5","crypto":{"ciphertext":"29c43c83b60a4fe76b26d2a724a440210037164136f03b9f5d67a4527d93b52a","cipherparams":{"iv":"506535b8298c83653a99e8326e2848e5"},"cipher":"aes-128-ctr","kdf":"pbkdf2","kdfparams":{"dklen":32,"salt":"03274eacc0f28b7bcaf5086707e9792b40573857e4000d013c70a1c8ca53b911","c":10240,"prf":"hmac-sha256"},"mac":"1d915b45d2d78197df3ba136e4b8b551356d672942b99dd5b6aa71e2b01a1c26"}}'
   );
-  fakeAccount.balance = '312021173902076846';
-  fakeAccount.nonce = 22;
-  fakeAccount.manualNonce = 22;
+  fakeAccount.balance = "1000000000000000000";
+  fakeAccount.nonce = 0;
+  fakeAccount.manualNonce = 0;
 
   const storeState = {
     connection: {
@@ -125,11 +125,11 @@ it('import new account pending', () => {
   };
 
   return expectSaga(importNewAccount, {payload : { 
-    address: account.address, 
-    type: account.type, 
-    keystring: account.keystring, 
+    address: fakeAccount.address, 
+    type: fakeAccount.type, 
+    keystring: fakeAccount.keystring, 
     ethereum: ethereum, 
-    avatar: account.avatar, 
+    avatar: fakeAccount.avatar, 
     tokens: tokens
   }})
   .withState(storeState)
