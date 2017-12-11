09ac4ffde92736ec0f47ff7ec8cede6378d66826
Tuan Nguyen <mtuan.94@outlook.com>
2017-12-11 10:35:50 +0700

    add translate for info kyber

--- lang/index.js
+++ lang/index.js
@@ -1,7 +1,9 @@
 
 const loadAll = false
 const defaultLanguage = 'en'
-const supportLanguage = [defaultLanguage, 'vi', 'fr']
 const otherLang = ['vi', 'fr']
+
+const supportLanguage = [defaultLanguage, ...otherLang]
+
 const defaultAndActive = [defaultLanguage, 'active']
 module.exports = { supportLanguage, defaultLanguage, loadAll, defaultAndActive, otherLang }
