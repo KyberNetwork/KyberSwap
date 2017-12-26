745345b8c7a3ef4ad0bc0504e9bd6221b3e5a5fb
Andrew <ncuong.bkhn@gmail.com>
2017-12-26 16:49:20 +0700

    fix modal info size

--- src/js/components/InfoKyber.js
+++ src/js/components/InfoKyber.js
@@ -18,7 +18,7 @@ export default class InfoKyber extends React.Component {
       <div id="info">
         <div class="frame">
           <div class="row">
-            <div class="column small-11 medium-9 large-8 small-centered">
+            <div class="column small-11 medium-11 large-11 small-centered">
               <p class="info">{this.props.translate("info.version") || "Version"}: 0.0.1<br />{this.props.translate("info.chain") || "Chain"}: Kovan
               <label>{this.props.translate("info.node_endpoint") || "Node endpoint"}
                 <input class="address" type="text" value={BLOCKCHAIN_INFO.endpoint} readOnly="true" />
