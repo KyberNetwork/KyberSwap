186162b3f79b5d7246d1a2ea0df85965c81c06c0
Tran Bao Huy <huy23121994@gmail.com>
2017-12-29 13:59:24 +0700

    update css transaction config

--- src/js/containers/Exchange/MinRate.js
+++ src/js/containers/Exchange/MinRate.js
@@ -5,16 +5,16 @@ import * as converter from "../../utils/converter"
 import * as actions from "../../actions/exchangeActions"
 
 @connect((store) => {
-  return {exchange: store.exchange}
+  return { exchange: store.exchange }
 })
 
 export default class MinRate extends React.Component {
 
   changeOfferRate = (e) => {
     var value = e.target.value
-    if (value === "" || isNaN(value)){
-      this.props.dispatch(actions.setOfferedRate(value))  
-    }else{
+    if (value === "" || isNaN(value)) {
+      this.props.dispatch(actions.setOfferedRate(value))
+    } else {
       var valueB = converter.toTWei(value)
       this.props.dispatch(actions.setOfferedRate(valueB.toString()))
     }
@@ -23,14 +23,19 @@ export default class MinRate extends React.Component {
 
   render = () => {
     var offeredRate = this.props.exchange.offeredRate
-    if (offeredRate !== "" && !isNaN(offeredRate)){
+    if (offeredRate !== "" && !isNaN(offeredRate)) {
       offeredRate = converter.toT(offeredRate, 18)
     }
     return (
-      <div className="min-rate">
-        <span>Min Rate</span>
-        <input type="number" value={offeredRate} onChange={(e) => this.changeOfferRate(e)}/>
-        <span className="error">{this.props.exchange.errors.rateError}</span>
+      <div className="row min-rate small-12 medium-8">
+        <label className="column small-12 medium-3 text-right">
+          <span>Min Rate</span>
+          <span className="k k-info k-2x ml-3" title="Increase for better profit"></span>
+        </label>
+        <div className="column small-12 medium-6 end p-relative">
+          <input type="number" value={offeredRate} onChange={(e) => this.changeOfferRate(e)} />
+          <span className="error-text">{this.props.exchange.errors.rateError}</span>
+        </div>
       </div>
     )
   }
