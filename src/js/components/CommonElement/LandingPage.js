1d6a2cfce334e9251fd5ee309b5285c015600895
Tran Bao Huy <huy23121994@gmail.com>
2018-01-09 16:43:14 +0700

    add landingpage

---
+++ src/js/components/CommonElement/LandingPage.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const LandingPage = (props)=> {
+  return (
+    <div class="frame" id="get-start">
+        <div className="row">
+            <div className="column text-center">
+                <h3 class="title">Trust-Free Exchange  for Ethereum tokens</h3>
+                    <ul class="show-for-medium">
+                        <li class="">Trustless</li>
+                        <li class="">Instant</li>
+                        <li class="">Liquid</li>
+                        <li class="">Compatible</li>
+                    </ul>
+                <button class="button accent">Get Started</button>
+            </div>
+        </div>
+    </div>
+  )
+  
+}
+export default LandingPage
