7feeac78b256f1256760850ccf7f1c54af9ec61b
Tran Bao Huy <huy23121994@gmail.com>
2018-01-13 13:50:37 +0700

    update test code

--- testing/container/ImportAccount/ImportByMetamask.spec.js
+++ testing/container/ImportAccount/ImportByMetamask.spec.js
@@ -2,6 +2,7 @@ import store from '../store';
 import React from 'react';
 import ImportByMetamask from '../../../src/js/containers/ImportAccount/ImportByMetamask';
 import BigNumber from "bignumber.js";
+import { getTranslate } from 'react-localize-redux';
 
 import { shallow, mount } from 'enzyme';
 
@@ -34,13 +35,19 @@ describe('test connect metamask', () => {
                     {
                         "address": "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", 
                         "balance": new BigNumber(Math.pow(10, 19)), 
-                        "decimal": 18, "icon": "/assets/img/tokens/eth.svg", "name": "Ethereum", "symbol": "ETH"},
+                        "decimal": 18, "icon": "/assets/img/tokens/eth.svg", "name": "Ethereum", "symbol": "ETH",
+                        "minRate": '0',
+                        "minRateETH": '0'
+                    },
                     {
                         "address": "0x1795b4560491c941c0635451f07332effe3ee7b3", 
                         "balance": new BigNumber(Math.pow(10, 18)), 
-                        "decimal": 9, "icon": "/assets/img/tokens/omg.svg", "name": "OmiseGO","symbol": "OMG"
+                        "decimal": 9, "icon": "/assets/img/tokens/omg.svg", "name": "OmiseGO","symbol": "OMG",
+                        "minRate": '21699393893477488',
+                        "minRateETH": '43391211520165175427',
                     }
                 ], 
+                translate: getTranslate(store.getState().locale),
                 "web3Service": ''
             }
         });
