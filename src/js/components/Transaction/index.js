daf1ce7c4e3a42cfbad63b2221817a14e04af406
Tran Bao Huy <tranbaohuy@Trans-MacBook-Pro.local>
2018-01-15 19:28:11 +0700

    add analyze modal

--- src/js/components/Transaction/index.js
+++ src/js/components/Transaction/index.js
@@ -9,3 +9,4 @@ export { default as PostTransferBtn } from './PostTransferBtn.js';
 
 export { default as TransactionConfig } from './TransactionConfig.js'
 export { default as TransactionLoadingView } from "./TransactionLoadingView.js"
+export { default as AnalyzeLogModal } from "./AnalyzeLogModal.js"
