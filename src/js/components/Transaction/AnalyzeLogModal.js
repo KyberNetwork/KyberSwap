547e567221859eae1afd1eb96fd69e674c0ba56d
Tran Bao Huy <huy23121994@gmail.com>
2018-01-24 19:34:24 +0700

    update no reason anlyze message

--- src/js/components/Transaction/AnalyzeLogModal.js
+++ src/js/components/Transaction/AnalyzeLogModal.js
@@ -10,12 +10,17 @@ const AnalyzelogModal = (props) => {
         </div>
     )
     if (props.analyze.isAnalizeComplete) {
-        listErrors = Object.keys(props.analyze.analizeError).map(key => {
-            var list = Object.keys(props.analyze.analizeError[key]).map(keyItem => {
-                return (<div key={key + keyItem} className="reason-item">{props.analyze.analizeError[key][keyItem]}</div>)
+        var isEmpty = true
+        listErrors = Object.keys(x).map(key => {
+            if(Object.keys(x[key]).length > 0) isEmpty = false
+            var list = Object.keys(x[key]).map(keyItem => {
+                return (<div key={key + keyItem} className="reason-item">{x[key][keyItem]}</div>)
             })
             return list
         })
+        listErrors = isEmpty ? (
+            <div className="reason-item">{props.translate('no_reason_found') || 'No any reason was found'}</div>
+        ) : listErrors
     }
 
     const content = (
