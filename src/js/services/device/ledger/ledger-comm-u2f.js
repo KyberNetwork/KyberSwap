01702c74e5ab1472b089d9400a0e88dd0eb1b666
Tran Bao Huy <huy23121994@gmail.com>
2018-01-26 19:48:57 +0700

    detect Ledger error message

--- src/js/services/device/ledger/ledger-comm-u2f.js
+++ src/js/services/device/ledger/ledger-comm-u2f.js
@@ -97,7 +97,7 @@ Ledger3.prototype.close_async = function() {
 
 Ledger3.create_async = function(timeout) {
 	return Q.fcall(function() {
-		return new Ledger3(timeout || 20);
+		return new Ledger3(timeout || 10);
 	});	
 }
 
