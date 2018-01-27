0a2649d3e68001ad8ea476c865d0219b8a5b6b54
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Pro.local>
2018-01-28 00:15:53 +0700

    fix message

--- src/js/containers/Exchange/PostExchange.js
+++ src/js/containers/Exchange/PostExchange.js
@@ -51,10 +51,10 @@ import { RateBetweenToken } from "../Exchange"
 
 export default class PostExchange extends React.Component {
   clickExchange = () => {
-    if(!this.props.form.kyber_enabled){
-      this.props.dispatch(utilActions.openInfoModal("transaction.notification", "transaction.kyber_down"))
-      return
-    }
+    // if(!this.props.form.kyber_enabled){
+    //   this.props.dispatch(utilActions.openInfoModal(this.props.translate("transaction.notification"), this.props.translate("transaction.kyber_down")))
+    //   return
+    // }
     if(this.props.form.maxCap == 0){
       let titleModal = this.props.translate('transaction.transaction_error') || 'Transaction error'
       let contentModal = this.props.translate('transaction.not_enable_exchange') || 'Your address is not enabled for exchange'
