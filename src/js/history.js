d2dd09a0025fae059fa55f2eada950a128db64df
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2018-03-03 22:52:12 +0700

    add_ga

--- src/js/history.js
+++ src/js/history.js
@@ -6,4 +6,10 @@ const history = createHistory({
     basename: '',           
     hashType: 'slash',          
   })
+history.listen(function (location) {
+  if (typeof window.ga === 'function') {
+    window.ga('set', 'page', location.pathname + location.search)
+    window.ga('send', 'pageview', location.pathname + location.search)
+  }
+})
 export default history
