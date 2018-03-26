0285e48794d8eee065cdec6bf7fc84cce3de6a0d
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2018-03-26 11:35:42 +0700

    add address cipher

--- src/js/services/web3.js
+++ src/js/services/web3.js
@@ -69,7 +69,7 @@ export class Web3Service {
 export function getWalletId(walletType, blockNo){
     switch(walletType){
         case "cipher":
-          return converters.numberToHexAddress(blockNo)
+          return "0xdd61803d4a56c597e0fc864f7a20ec7158c6cba5"
           break
         case "trust":
           return "0xf1aa99c69715f423086008eb9d06dc1e35cc504d"
