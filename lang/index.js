b5a93d0db3f247aed0b97616ecb7b83f979ae562
komninoschat <komninoschat@gmail.com>
2018-06-02 00:32:53 +0300

    Add Greek to language index

--- lang/index.js
+++ lang/index.js
@@ -1,7 +1,7 @@
 
 const loadAll = true
 const defaultLanguage = 'en'
-const otherLang = ['cn', 'kr' , 'vi']
+const otherLang = ['el', 'cn', 'kr' , 'vi']
 
 const supportLanguage = [defaultLanguage, ...otherLang]
 
