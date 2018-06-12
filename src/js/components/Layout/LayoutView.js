8f25dddb7425d57668885932adcd9024077d8ec6
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2018-06-12 16:22:52 +0700

    change root location

--- src/js/components/Layout/LayoutView.js
+++ src/js/components/Layout/LayoutView.js
@@ -3,6 +3,8 @@ import { Switch, Route, Redirect } from 'react-router'
 import { ConnectedRouter } from 'react-router-redux'
 import { Processing, InfoModal } from "../../containers/CommonElements/"
 import { Link } from 'react-router-dom'
+
+import constansts from "../../services/constants"
 //import { Rate } from "../Header"
 
 const LayoutView = (props) => {
@@ -12,11 +14,9 @@ const LayoutView = (props) => {
         <Route component={props.Header} />
         <section id="content">
           <Switch>
-            <Route exact path="/" component={props.ImportAccount} />
-            <Route exact path="/info" component={props.InfoKyber} />
-            <Route exact path="/exchange" component={props.Exchange} />
-            <Route exact path="/transfer" component={props.Transfer} />
-            <Redirect from="*" to="/" />
+            <Route exact path={constansts.BASE_HOST} component={props.ImportAccount} />
+            <Route exact path={constansts.BASE_HOST + "/exchange"} component={props.Exchange} />
+            <Route exact path={constansts.BASE_HOST + "/transfer"} component={props.Transfer} />            
           </Switch>
           {/* <div id="rate-bar" class="mb-8">
             {props.rate}
