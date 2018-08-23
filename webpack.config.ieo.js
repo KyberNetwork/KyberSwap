976eceded39cb9d43b9cf881cbaeab08de6b4e4f
NNagato <darkdragon236@gmail.com>
2018-08-23 15:27:12 +0700

    update webpack config

--- webpack.config.ieo.js
+++ webpack.config.ieo.js
@@ -96,6 +96,7 @@ var scriptConfig = function (env) {
         new WebpackShellPlugin(
             {
                 // hash: hash,
+                onBuildStart:['node webpack.beforebuild.js'],
                 onBuildEnd:[`BUNDLE_NAME=[hash] chain=${chain} node webpack.afterbuild.js`]
             }
         )
