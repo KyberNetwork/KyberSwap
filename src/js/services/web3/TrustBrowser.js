0ec1f4a7e23e59493b81ea86d8144178f2874de6
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2018-10-24 14:59:08 +0700

    generalize web3 interface

---
+++ src/js/services/web3/TrustBrowser.js
@@ -0,0 +1,11 @@
+
+import DappBrowser from "DappBrowser.js"
+
+export default class TrustBrowser extends DappBrowser {
+
+
+  getWalletId(){
+    return "0xf1aa99c69715f423086008eb9d06dc1e35cc504d"
+  }
+
+}
