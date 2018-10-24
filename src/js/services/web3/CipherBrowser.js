bc5eac5a79042f78d1a8eec198fafb4b089d20ab
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2018-10-24 14:59:08 +0700

    generalize web3 interface

---
+++ src/js/services/web3/CipherBrowser.js
@@ -0,0 +1,11 @@
+
+import DappBrowser from "DappBrowser.js"
+
+export default class CipherBrowser extends DappBrowser {
+
+
+  getWalletId(){
+    return "0xdd61803d4a56c597e0fc864f7a20ec7158c6cba5"
+  }
+
+}
