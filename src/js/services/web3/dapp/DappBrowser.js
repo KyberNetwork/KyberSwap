3c09f13ffb3667ec839d250358c2017ff2442f2b
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2018-12-18 14:15:05 +0700

    build

--- src/js/services/web3/dapp/DappBrowser.js
+++ src/js/services/web3/dapp/DappBrowser.js
@@ -10,7 +10,7 @@ export default class DappBrowser {
   constructor() {
     this.web3 = new Web3(Web3.givenProvider || window.web3.currentProvider || window.web3.givenProvider)
     //for older verions of web3
-    console.log("web3_v2")
+    console.log("web3_v3")
     if (this.web3 && this.web3.net && !this.web3.eth.net){
       this.web3.eth.net = this.web3.net
     }
