4ec78163a8e9e8cd82d9b03d8f46658cb0850b7c
NNagato <darkdragon236@gmail.com>
2019-01-21 11:00:46 +0700

    fix css exchange layout

--- src/js/components/Exchange/index.js
+++ src/js/components/Exchange/index.js
@@ -2,4 +2,3 @@ export { default as ExchangeBodyLayout } from "./ExchangeBodyLayout.js"
 export { default as PostExchangeBtn } from "./PostExchangeBtn.js"
 
 export { default as AccountBalanceLayout } from './AccountBalanceLayout.js'
-export { default as BigInput } from './BigInput.js'
