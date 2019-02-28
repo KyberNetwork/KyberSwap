94cf5f44abac01140a39510a1c7e141d9681cf81
Thang Dinh <vietthang207@gmail.com>
2019-02-28 14:28:18 +0700

    add awx job launch

--- .travis/awx_job_launch.sh
+++ .travis/awx_job_launch.sh
@@ -5,15 +5,21 @@ set -euo pipefail
 readonly awx_username=${AWX_USERNAME:-}
 readonly awx_password=${AWX_PASSWORD:-}
 readonly awx_develop_job_launch_uri=${AWX_DEVELOP_JOB_LAUNCH_URI:-}
-readonly awx_master_job_launch_uri=${AWX_MASTER_JOB_LAUNCH_URI:-}
+readonly awx_staging_job_launch_uri=${AWX_STAGING_JOB_LAUNCH_URI:-}
 
-if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+if [[ "$SWAP_SERVER" == "develop" ]]; then
     awx_job_launch_uri=$awx_develop_job_launch_uri
-elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
-    awx_job_launch_uri=$awx_master_job_launch_uri
+elif [[ "$SWAP_SERVER" == "staging" ]]; then
+    awx_job_launch_uri=$awx_staging_job_launch_uri
 else
-    echo "branch is not set to call awx deploy"
+    echo "swap server is not set to call awx deploy"
     exit 0
 fi
 
-curl -X POST -o /dev/null -w "HTTP response code: %{http_code}\n" $awx_job_launch_uri --user "$awx_username:$awx_password"
+# VERY IMPORTANT: "prompt on launch" need to be set in the job template config in order for this API call to work.
+curl -X POST $awx_job_launch_uri \
+    --output /dev/null \
+    --write-out "HTTP response code: %{http_code}\n" \
+    --user "$awx_username:$awx_password" \
+    --header "Content-Type: application/json" \
+    --data '{ "extra_vars": {"kyber_swap_revision": "'$TRAVIS_COMMIT'", "kyber_swap_env": "'$SWAP_ENV'"}}'
