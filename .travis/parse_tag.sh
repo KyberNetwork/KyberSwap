34da474032f45d5d2424234f224daef06c89e80e
Thang Dinh <vietthang207@gmail.com>
2019-03-08 14:36:17 +0700

    improve message printing in parse_tag.sh

--- .travis/parse_tag.sh
+++ .travis/parse_tag.sh
@@ -12,7 +12,7 @@ else
     if [[ $TRAVIS_TAG =~ $regex ]]; then
         export SWAP_SERVER="${BASH_REMATCH[1]}"
         export SWAP_ENV="${BASH_REMATCH[2]}"
-        echo server: $SWAP_SERVER env: $SWAP_ENV
+        printf 'server: %s env: %s' "$SWAP_SERVER" "$SWAP_ENV"
     fi
 fi
 
