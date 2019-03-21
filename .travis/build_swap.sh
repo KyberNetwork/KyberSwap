bf25dc71981d7f049029466c139e4d54c48196bc
Thang Dinh <vietthang207@gmail.com>
2019-03-21 11:23:18 +0700

    fix building process

--- .travis/build_swap.sh
+++ .travis/build_swap.sh
@@ -2,12 +2,5 @@
 
 set -euo pipefail
 
-TRAVIS_TAG=${TRAVIS_TAG:-}
-
-if [[ -z "$TRAVIS_TAG" ]]; then
-    echo 'TRAVIS_TAG is not set.'
-    exit 0
-else
-    echo "Building swap from commit $TRAVIS_COMMIT with env $SWAP_ENV"
-    npm run build-$SWAP_ENV
-fi
+echo "Building swap from commit $TRAVIS_COMMIT with env $SWAP_ENV"
+npm run build-all
