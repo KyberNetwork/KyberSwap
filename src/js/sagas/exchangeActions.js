dfd073c4e9464e46d4eb7a52bf479a63d058ccc8
Nguyen Le Quang <quangkeu95@gmail.com>
2019-04-26 15:17:35 +0700

    Hot fix rate update

--- src/js/sagas/exchangeActions.js
+++ src/js/sagas/exchangeActions.js
@@ -1043,9 +1043,9 @@ function* updateRatePending(action) {
 
 function* updateRateAndValidateSource(action) {
   const state = store.getState();
-  const { sourceAmount } = action.payload;
+  const { ethereum, source, dest, sourceValue, sourceTokenSymbol, sourceAmount, isManual, refetchSourceAmount } = action.payload;
   try {
-    yield call(updateRatePending, action);
+    yield put(actions.updateRateExchange(ethereum, source, dest, sourceAmount, sourceTokenSymbol, isManual, refetchSourceAmount));
     if (state.account.account !== false) {
       yield call(verifyExchange);
     }
