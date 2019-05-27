e4f0db4e6c313ad62961cb6bcfe87d2c73d3ffd2
Thang Dinh <vietthang207@gmail.com>
2019-05-27 16:55:13 +0700

    add staging and remove deployment trigger by tag

--- .travis/awx_job_launch.sh
+++ .travis/awx_job_launch.sh
@@ -9,17 +9,15 @@ readonly awx_staging_job_launch_uri=${AWX_STAGING_JOB_LAUNCH_URI:-}
 
 if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
     awx_job_launch_uri=$awx_develop_job_launch_uri
-elif [[ "$TRAVIS_TAG" == "deploy_staging" ]]; then
+elif [[ "$TRAVIS_BRANCH" == "staging" ]]; then
     awx_job_launch_uri=$awx_staging_job_launch_uri
 else
-    echo "Branch is not develop and deploy_staging tag is not set."
+    echo "Branch is not set for autodeployment."
     exit 0
 fi
 
-# VERY IMPORTANT: "prompt on launch" need to be set in the job template config in order for this API call to work.
 curl -X POST $awx_job_launch_uri \
     --output /dev/null \
     --write-out "HTTP response code: %{http_code}\n" \
     --user "$awx_username:$awx_password" \
     --header "Content-Type: application/json" \
-    --data '{ "extra_vars": {"kyber_swap_revision": "'$TRAVIS_BRANCH'"}}'
