cf5ad0f244e03e3bc89b848015be5a11275e6cbe
Nguyen Le Quang <quangkeu95@gmail.com>
2019-06-15 15:33:23 +0700

    Change layout

--- src/js/containers/Transfer/TransferModals/ConfirmModal.js
+++ src/js/containers/Transfer/TransferModals/ConfirmModal.js
@@ -246,7 +246,9 @@ export default class ConfirmModal extends React.Component {
     contentModal = () => {
         return (
             <div>
-                <a className="x" onClick={this.closeModal}>&times;</a>
+                <a className="x" onClick={this.closeModal}>
+                    <img src={require("../../../assets/img/v3/Close-3.svg")} />
+                </a>
                 <div className="content with-overlap">
                     <div className="row">
                         <div>
