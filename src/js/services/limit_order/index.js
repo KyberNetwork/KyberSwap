5539bed281cf8c57998ae7bf0cae918c69dda9d6
Nguyen Le Quang <quangkeu95@gmail.com>
2019-07-09 17:36:27 +0700

    Fix import

--- src/js/services/limit_order/index.js
+++ src/js/services/limit_order/index.js
@@ -1,8 +1,9 @@
 
-
-import * as limit_order from "./limit_order"
-import * as limit_order_mock from "./limit_order_mock"
-
-const limitOrderService = process.env && process.env.integrate ? limit_order : limit_order_mock;
+let limitOrderService
+if (process.env && process.env.integrate) {
+  limitOrderService = require("./limit_order");
+} else {
+  limitOrderService = require("./limit_order_mock");
+}
 
 export default limitOrderService;
