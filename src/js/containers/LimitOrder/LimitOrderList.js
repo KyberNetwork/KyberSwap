f40adee112e2240cbf9a8912ecceb3f8633e6282
Nguyen Le Quang <quangkeu95@gmail.com>
2019-07-10 21:19:15 +0700

    Add faq title

--- src/js/containers/LimitOrder/LimitOrderList.js
+++ src/js/containers/LimitOrder/LimitOrderList.js
@@ -88,6 +88,11 @@ export default class LimitOrderList extends React.Component {
         <div>
           <div className="limit-order-list--title">
             <div className="title">{this.props.translate("limit_order.order_list_title") || "Manage Your Orders"}</div>
+            <div className="limit-order-list--title-faq">
+              <a href="/faq#I-submitted-the-limit-order-but-it-was-not-triggered-even-though-my-desired-price-was-hit" target="_blank">
+                {this.props.translate("limit_order.wonder_why_order_not_filled")}
+              </a>
+            </div>
             <div className="limit-order-list__filter-container">
               <ul className="filter">
                 {this.getTimeFilter()}
