71deba760842a70efff63d48776fed446d4b03d7
Nguyen Le Quang <quangkeu95@gmail.com>
2019-07-15 17:11:20 +0700

    Fix rate rounding format

--- src/js/containers/LimitOrder/LimitOrderForm.js
+++ src/js/containers/LimitOrder/LimitOrderForm.js
@@ -208,7 +208,7 @@ export default class LimitOrderForm extends React.Component {
 
     const tableComp = higherRateOrders.map(item => {
       const datetime = common.getFormattedDate(item.updated_at);
-      const rate = converters.roundingNumber(item.min_rate);
+      const rate = converters.roundingRateNumber(item.min_rate);
       return (
         <div key={item.id} className="rate-warning-tooltip__order">
           <div>{datetime}</div>
--- src/js/utils/converter.js
+++ src/js/utils/converter.js
@@ -412,7 +412,7 @@ export function roundingNumber(number) {
   number = +number;
   let numberStr = number.toString();
   if (isNaN(number) || number <= 0) number = 0;
-  if (number < 1e-9) number = 0;
+  if (number < 1e-7) number = 0;
   if (('' + Math.floor(number)).length >= MAX_DIGIS) {
     return Math.floor(number).toLocaleString();
   }
@@ -428,8 +428,43 @@ export function roundingNumber(number) {
 
   let minDisplay = MAX_DIGIS - count_0 < 4 ? 4 : MAX_DIGIS - count_0
 
-  // let precision = number.toPrecision((number < 1 && number > 0) ? minDisplay : MAX_DIGIS);
-  // precision = (precision * 1).toString();
+  let precision = number.toPrecision((number < 1 && number > 0) ? minDisplay : MAX_DIGIS);
+  precision = (precision * 1).toString();
+
+  let arr = precision.split('.'),
+    intPart = arr[0],
+    i = intPart.length % SIZE || SIZE,
+    result = intPart.substr(0, i);
+
+  for (; i < intPart.length; i += SIZE) {
+    result += ',' + intPart.substr(i, SIZE);
+  }
+  if (arr[1]) {
+    result += '.' + arr[1];
+  }
+  return result;
+}
+
+export function roundingRateNumber(number) {
+  var MAX_DIGIS = 7, SIZE = 3;
+  number = +number;
+  let numberStr = number.toString();
+  if (isNaN(number) || number <= 0) number = 0;
+  if (number < 1e-9) number = 0;
+  if (('' + Math.floor(number)).length >= MAX_DIGIS) {
+    return Math.floor(number).toLocaleString();
+  }
+
+  let count_0 = 0
+  for (let j of numberStr) {
+    if (j == '.') continue
+    if (j == 0)
+      count_0++
+    else
+      break
+  }
+
+  let minDisplay = MAX_DIGIS - count_0 < 4 ? 4 : MAX_DIGIS - count_0
 
   let precision
 
