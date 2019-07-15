a94e03647c1906c1435b4683f8240c9f18744e2c
trungnx <trung@kyber.network>
2019-07-15 10:44:41 +0700

    Detect Alpha Wallet and ImToken to remove prefix for signing

---
+++ src/js/services/web3/dapp/ImTokenBrowser.js
@@ -0,0 +1,22 @@
+import DappBrowser from "./DappBrowser"
+import * as ethUtil from "ethereumjs-util";
+
+export default class ImTokenBrowser extends DappBrowser {
+  async sign(message) {
+    try {
+      var account = await this.getCoinbase(true)
+      var signature = await this.web3.eth.sign(message, account)
+
+      var {v, r, s} = ethUtil.fromRpcSig(signature)
+      r = ethUtil.bufferToHex(r)
+      s = ethUtil.bufferToHex(s)
+
+      signature = ethUtil.toRpcSig(v, r, s)
+
+      return signature
+    } catch(err) {
+      console.log(err)
+      throw err
+    }
+  }
+}
