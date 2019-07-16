06953888d361fb28636a126e32839dcf8ea46ba2
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2019-07-16 11:19:28 +0700

    fix sign with alpha wallet

--- src/js/services/web3/dapp/AlphaWalletBrowser.js
+++ src/js/services/web3/dapp/AlphaWalletBrowser.js
@@ -2,10 +2,24 @@ import DappBrowser from "./DappBrowser"
 import * as ethUtil from "ethereumjs-util";
 
 export default class AlphaWalletBrowser extends DappBrowser {
+
+  personalCall = (message, account) => {
+    return new Promise((resolve, reject)=>{
+      web3.personal.sign(message, account, (error, result)=>{
+        if(!error){
+          resolve(result)
+        }else{
+          reject(error)
+        }
+      })
+    })
+  }
+
   async sign(message) {
     try {
       var account = await this.getCoinbase(true)
-      var signature = await this.web3.eth.sign(message, account)
+      
+      var signature = await this.personalCall(message, account)
 
       var {v, r, s} = ethUtil.fromRpcSig(signature)
       r = ethUtil.bufferToHex(r)
