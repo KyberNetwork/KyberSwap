739313764aec26f336606848d68cc4b9fab9d82f
Nguyen Le Quang <quangkeu95@gmail.com>
2019-07-17 09:53:06 +0700

    Fix signing alpha and imtoken wallet

--- src/js/services/web3/dapp/AlphaWalletBrowser.js
+++ src/js/services/web3/dapp/AlphaWalletBrowser.js
@@ -3,34 +3,34 @@ import * as ethUtil from "ethereumjs-util";
 
 export default class AlphaWalletBrowser extends DappBrowser {
 
-  personalCall = (message, account) => {
-    return new Promise((resolve, reject)=>{
-      web3.personal.sign(message, account, (error, result)=>{
-        if(!error){
-          resolve(result)
-        }else{
-          reject(error)
-        }
-      })
-    })
-  }
+  // personalCall = (message, account) => {
+  //   return new Promise((resolve, reject)=>{
+  //     web3.personal.sign(message, account, (error, result)=>{
+  //       if(!error){
+  //         resolve(result)
+  //       }else{
+  //         reject(error)
+  //       }
+  //     })
+  //   })
+  // }
 
-  async sign(message) {
-    try {
-      var account = await this.getCoinbase(true)
+  // async sign(message) {
+  //   try {
+  //     var account = await this.getCoinbase(true)
       
-      var signature = await this.personalCall(message, account)
+  //     var signature = await this.personalCall(message, account)
 
-      var {v, r, s} = ethUtil.fromRpcSig(signature)
-      r = ethUtil.bufferToHex(r)
-      s = ethUtil.bufferToHex(s)
+  //     var {v, r, s} = ethUtil.fromRpcSig(signature)
+  //     r = ethUtil.bufferToHex(r)
+  //     s = ethUtil.bufferToHex(s)
 
-      signature = ethUtil.toRpcSig(v, r, s)
+  //     signature = ethUtil.toRpcSig(v, r, s)
 
-      return signature
-    } catch(err) {
-      console.log(err)
-      throw err
-    }
-  }
+  //     return signature
+  //   } catch(err) {
+  //     console.log(err)
+  //     throw err
+  //   }
+  // }
 }
--- src/js/services/web3/dapp/ImTokenBrowser.js
+++ src/js/services/web3/dapp/ImTokenBrowser.js
@@ -2,21 +2,21 @@ import DappBrowser from "./DappBrowser"
 import * as ethUtil from "ethereumjs-util";
 
 export default class ImTokenBrowser extends DappBrowser {
-  async sign(message) {
-    try {
-      var account = await this.getCoinbase(true)
-      var signature = await this.web3.eth.sign(message, account)
+  // async sign(message) {
+  //   try {
+  //     var account = await this.getCoinbase(true)
+  //     var signature = await this.web3.eth.sign(message, account)
 
-      var {v, r, s} = ethUtil.fromRpcSig(signature)
-      r = ethUtil.bufferToHex(r)
-      s = ethUtil.bufferToHex(s)
+  //     var {v, r, s} = ethUtil.fromRpcSig(signature)
+  //     r = ethUtil.bufferToHex(r)
+  //     s = ethUtil.bufferToHex(s)
 
-      signature = ethUtil.toRpcSig(v, r, s)
+  //     signature = ethUtil.toRpcSig(v, r, s)
 
-      return signature
-    } catch(err) {
-      console.log(err)
-      throw err
-    }
-  }
+  //     return signature
+  //   } catch(err) {
+  //     console.log(err)
+  //     throw err
+  //   }
+  // }
 }
