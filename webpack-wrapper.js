3350d813e3a8393f1b0075026dd9b7308397839f
Nguyen Le Quang <quangkeu95@gmail.com>
2019-07-29 17:57:33 +0700

    Minify image webpack

--- webpack-wrapper.js
+++ webpack-wrapper.js
@@ -124,6 +124,13 @@ var getConfig = env => {
                             }
                         }
                     ]
+                }, {
+                    test: /\.(jpg|png|gif|svg)$/,
+                    loader: 'image-webpack-loader',
+                    // Specify enforce: 'pre' to apply the loader
+                    // before url-loader/svg-url-loader
+                    // and not duplicate it in rules with them
+                    enforce: 'pre'
                 }
             ]
         },
