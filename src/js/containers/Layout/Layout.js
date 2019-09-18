7646c5eb7390b102e70ab91d7325aba19c696477
Vu Tien <tien7668@gmail.com>
2019-09-18 15:10:35 +0700

    integrate user-dashboard

--- src/js/containers/Layout/Layout.js
+++ src/js/containers/Layout/Layout.js
@@ -18,6 +18,7 @@ import Language from "../../../../lang"
 import AnalyticFactory from "../../services/analytics"
 import BLOCKCHAIN_INFO from "../../../../env";
 
+import { Redirect } from 'react-router-dom'
 @connect((store) => {
   var locale = store.locale
   var code
@@ -46,6 +47,14 @@ import BLOCKCHAIN_INFO from "../../../../env";
       langClass = ""
   }
 
+  const exchange = store.exchange
+  const transfer = store.transfer
+  const limitOrder = store.limitOrder
+
+  var exchangeLink = constanst.BASE_HOST + "/swap/" + exchange.sourceTokenSymbol.toLowerCase() + "-" + exchange.destTokenSymbol.toLowerCase()
+  var transferLink = constanst.BASE_HOST + "/transfer/" + transfer.tokenSymbol.toLowerCase()
+  var orderLink = constanst.BASE_HOST + `/${constanst.LIMIT_ORDER_CONFIG.path}/` + limitOrder.sourceTokenSymbol.toLowerCase() + "-" + limitOrder.destTokenSymbol.toLowerCase()
+
   return {
     ethereumNode: store.connection.ethereum,
     currentBlock: store.global.currentBlock,
@@ -58,7 +67,8 @@ import BLOCKCHAIN_INFO from "../../../../env";
     tokens: store.tokens.tokens,
     analytics: store.global.analytics,
     langClass: langClass,
-    theme: store.global.theme
+    theme: store.global.theme,
+    exchangeLink, transferLink, orderLink,
   }
 })
 
@@ -70,6 +80,8 @@ export default class Layout extends React.Component {
     this.idleMode = false;
     this.intervalIdle = null;
     this.LimitOrderAccount = withSourceAndBalance(<LimitOrderAccount />)
+    this.state = {redirect: false}
+
   }
 
   componentWillMount() {
@@ -99,6 +111,9 @@ export default class Layout extends React.Component {
     if (window.kyberBus) {
       window.kyberBus.on('set.theme.light', this.switchTheme('light'));
       window.kyberBus.on('set.theme.dark', this.switchTheme('dark'));
+      window.kyberBus.on('go.to.swap', () => {console.log('swap'); history.push(this.props.exchangeLink)});
+      window.kyberBus.on('go.to.transfer', () =>{console.log('transfer'); history.push(this.props.transferLink)});
+      window.kyberBus.on('go.to.limit_order', () => {console.log('limit_order'); history.push(this.props.orderLink)});
     }
   }
 
@@ -137,23 +152,20 @@ export default class Layout extends React.Component {
   setActiveLanguage = (language) => {
     this.props.dispatch(changeLanguage(this.props.ethereumNode, language, this.props.locale))
   }
-
   render() {
     var currentLanguage = common.getActiveLanguage(this.props.locale.languages)
     return (
-      <div className={`theme theme--${this.props.theme}`}>
-        <LayoutView
-          history={history}        
+      <LayoutView
+          history={history}
           Exchange={Exchange}
           Transfer={Transfer}
           LimitOrder = {LimitOrder}
           supportedLanguages={Language.supportLanguage}
-          setActiveLanguage={this.setActiveLanguage}      
-          currentLanguage = {currentLanguage}  
+          setActiveLanguage={this.setActiveLanguage}
+          currentLanguage = {currentLanguage}
           tokens = {this.props.tokens}
           langClass = {this.props.langClass}
-        />
-      </div>
+      />
     )
   }
 }
