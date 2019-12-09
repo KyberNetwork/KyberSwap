84a5a8167dc669360825d6856e15bba6f5cf23e8
trungnx <trung@kyber.network>
2019-12-09 11:02:38 +0700

    Remove get block information to checking node connected

--- src/js/services/ethereum/nodeProviders/BaseProvider.js
+++ src/js/services/ethereum/nodeProviders/BaseProvider.js
@@ -24,7 +24,7 @@ export default class BaseProvider {
     
     isConnectNode() {
         return new Promise((resolve, reject) => {
-            this.rpc.eth.getBlock("latest", false).then((block) => {
+            this.rpc.eth.net.isListening().then(() => {
                 resolve(true)
             }).catch(() => {
                 reject(false)
