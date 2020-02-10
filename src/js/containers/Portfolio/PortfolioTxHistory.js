8a53be8676a1cab584232a107d151abacbacc632
trungnx <trung@kyber.network>
2020-02-10 12:03:51 +0700

    Move validate portfolio txs to API itself

--- src/js/containers/Portfolio/PortfolioTxHistory.js
+++ src/js/containers/Portfolio/PortfolioTxHistory.js
@@ -67,9 +67,15 @@ export default class PortfolioTxHistory extends React.Component {
     if (!this.props.address) return;
   
     this.setState({ loadingHistory: true });
-    let { data, count, in_queue } = await portfolioService.fetchAddressTxs(address, page);
+    let { data, totalTxs, inQueue, isError } = await portfolioService.fetchAddressTxs(address, page);
+    
+    this.setState({ loadingError: isError });
+    if (isError) {
+      this.setState({ loadingHistory: false });
+      return;
+    }
 
-    if (in_queue) {
+    if (inQueue) {
       this.fetchingTxsInterval = setInterval(async () => {
         await this.setTxHistory();
       }, 2000);
@@ -83,7 +89,7 @@ export default class PortfolioTxHistory extends React.Component {
     this.setState({
       historyTxs: data,
       loadingHistory: false,
-      pageTotal: Math.ceil(count / PORTFOLIO_TX_LIMIT)
+      pageTotal: Math.ceil(totalTxs / PORTFOLIO_TX_LIMIT)
     });
   }
   
@@ -103,33 +109,6 @@ export default class PortfolioTxHistory extends React.Component {
   
   getTokenDecimal(tokenSymbol) {
     return tokenSymbol !== 'ETH' ? this.props.tokens[tokenSymbol].decimals : 18;
-  }
-  
-  validateTransferTx(tx) {
-    return tx.transfer_token_symbol && tx.transfer_token_value && tx.transfer_from && tx.transfer_to && tx.hash;
-  }
-  
-  validateSwapTx(tx) {
-    return tx.swap_source_token && tx.swap_dest_token && tx.swap_source_amount && !isNaN(tx.swap_source_amount) && tx.swap_dest_amount && !isNaN(tx.swap_dest_amount) && tx.hash;
-  }
-  
-  validateApproveTx(tx) {
-    return tx.approve_token_symbol && tx.hash;
-  }
-  
-  validateUndefinedTx(tx) {
-    return tx.from && tx.to && tx.hash;
-  }
-  
-  checkValidTxsByDate(txs) {
-    let isValid = false;
-    for (let i = 0; i < txs.length; i++) {
-      if (txs[i] !== null) {
-        isValid = true;
-        break;
-      }
-    }
-    return isValid;
   }
   
   renderTransactionHistory() {
@@ -157,14 +136,10 @@ export default class PortfolioTxHistory extends React.Component {
     }
     
     return Object.keys(txs).map((date, index) => {
-      const txsByDate = this.renderTxByDate(txs[date]);
-      
-      if (!this.checkValidTxsByDate(txsByDate)) return null;
-      
       return (
         <div className={"portfolio__tx"} key={index}>
           <div className={"portfolio__tx-header theme__table-header"}>{date}</div>
-          {txsByDate}
+          {this.renderTxByDate(txs[date])}
         </div>
       )
     });
@@ -173,9 +148,6 @@ export default class PortfolioTxHistory extends React.Component {
   renderTxByDate(txs) {
     return txs.map((tx, index) => {
       if (tx.type === TX_TYPES.transfer) {
-        const isValidTx = this.validateTransferTx(tx);
-        if (!isValidTx) return null;
-        
         const transferTokenSymbol = tx.transfer_token_symbol;
         const transferTokenDecimal = this.getTokenDecimal(transferTokenSymbol);
         const transferValue = this.formatTxValue(tx.transfer_token_value, transferTokenDecimal);
@@ -186,9 +158,6 @@ export default class PortfolioTxHistory extends React.Component {
           return this.renderReceiveTx(tx.hash, transferValue, transferTokenSymbol, tx.transfer_from, index);
         }
       } else if (tx.type === TX_TYPES.swap) {
-        const isValidTx = this.validateSwapTx(tx);
-        if (!isValidTx) return null;
-        
         const srcSymbol = this.state.tokenAddresses[tx.swap_source_token.toLowerCase()];
         const destSymbol = this.state.tokenAddresses[tx.swap_dest_token.toLowerCase()];
         const srcDecimal = this.getTokenDecimal(srcSymbol);
@@ -198,14 +167,8 @@ export default class PortfolioTxHistory extends React.Component {
         
         return this.renderSwapTx(tx.hash, srcValue, srcSymbol, destValue, destSymbol, index);
       } else if (tx.type === TX_TYPES.approve) {
-        const isValidTx = this.validateApproveTx(tx);
-        if (!isValidTx) return null;
-        
         return this.renderApproveTx(tx.hash, tx.approve_token_symbol, index);
       } else if (tx.type === TX_TYPES.undefined) {
-        const isValidTx = this.validateUndefinedTx(tx);
-        if (!isValidTx) return null;
-        
         return this.renderUndefinedTx(tx.hash, tx.from, tx.to, index);
       }
       
--- src/js/services/portfolio/portfolioService.js
+++ src/js/services/portfolio/portfolioService.js
@@ -1,7 +1,62 @@
-import { PORTFOLIO_TX_LIMIT } from '../constants';
+import { PORTFOLIO_TX_LIMIT, TX_TYPES } from '../constants';
 import BLOCKCHAIN_INFO from "../../../../env";
 
 export async function fetchAddressTxs(address, page) {
   const response = await fetch(`${BLOCKCHAIN_INFO.portfolio_api}/transactions?address=${address}&page=${page}&limit=${PORTFOLIO_TX_LIMIT}`);
-  return await response.json();
+  const result = await response.json();
+  const isValidResult = validateResultObject(result);
+  
+  if (!isValidResult) return returnResponseObject([], 0, false, true);
+  
+  let txs = [];
+  for (let i = 0; i < result.data.length; i++) {
+    const tx = result.data[i];
+    let isValidTx = false;
+    
+    if (tx.type === TX_TYPES.transfer) {
+      isValidTx = validateTransferTx(tx);
+    } else if (tx.type === TX_TYPES.swap) {
+      isValidTx = validateSwapTx(tx);
+    } else if (tx.type === TX_TYPES.approve) {
+      isValidTx = validateApproveTx(tx);
+    } else if (tx.type === TX_TYPES.undefined) {
+      isValidTx = validateUndefinedTx(tx);
+    }
+  
+    if (isValidTx) txs.push(tx);
+  }
+  
+  return returnResponseObject(txs, result.count, result.in_queue);
+}
+
+function validateResultObject(result) {
+  return result.data && Array.isArray(result.data) && result.count && !isNaN(result.count) &&
+    result.in_queue !== undefined && result.error === null
+}
+
+function validateTransferTx(tx) {
+  return tx.transfer_token_symbol && tx.transfer_token_value && !isNaN(tx.transfer_token_value) &&
+    tx.transfer_from && tx.transfer_to && tx.hash;
+}
+
+function validateSwapTx(tx) {
+  return tx.swap_source_token && tx.swap_dest_token && tx.swap_source_amount && !isNaN(tx.swap_source_amount) &&
+    tx.swap_dest_amount && !isNaN(tx.swap_dest_amount) && tx.hash;
+}
+
+function validateApproveTx(tx) {
+  return tx.approve_token_symbol && tx.hash;
+}
+
+function validateUndefinedTx(tx) {
+  return tx.from && tx.to && tx.hash;
+}
+
+function returnResponseObject(txs, totalTxs, inQueue, isError = false) {
+  return {
+    data: txs,
+    totalTxs,
+    inQueue,
+    isError
+  };
 }
