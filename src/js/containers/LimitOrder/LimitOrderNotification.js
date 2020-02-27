639ce787a429e9442c22865f2b4d943d79936db2
trungnx <trung@kyber.network>
2020-02-27 14:07:13 +0700

    Change discount fee for LO

--- src/js/containers/LimitOrder/LimitOrderNotification.js
+++ src/js/containers/LimitOrder/LimitOrderNotification.js
@@ -11,7 +11,7 @@ const LimitOrderNotification = (props) => {
   return (
     <div className="limit-order-notification theme__background-12">
       <div className="limit-order-notification__text">
-        {props.translate('limit_order.free_fee') || 'Gas free swaps (No fees) if you hold 2000 KNC in your wallet'}
+        {props.translate('limit_order.free_fee') || 'No fee for Limit Order (10 LOs per day) If you hold 2000 KNC in your wallet'}
       </div>
       <div className="limit-order-notification__button" onClick={goToSwap}>
         {props.translate('limit_order.hold_knc') || 'Hold 2000 KNC now'}
