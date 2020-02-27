73d3007b591466b09fd8b18f8129651e190eece3
trungnx <trung@kyber.network>
2020-02-27 18:18:42 +0700

    Fix buy rate LO form is different on the same input

--- src/js/containers/LimitOrder/LimitOrderSubmit.js
+++ src/js/containers/LimitOrder/LimitOrderSubmit.js
@@ -211,11 +211,11 @@ export default class LimitOrderSubmit extends React.Component {
         const isSamePair = sourceToken.symbol === item.source && destToken.symbol === item.dest;
         
         if (isSamePair) {
-          const rateComparison = converters.compareTwoNumber(item.min_rate, triggerRate) > 0;
+          const formattedRate = this.props.isBuyForm ? converters.formatNumberByPrecision(triggerRate, 18) : triggerRate;
+          const rateComparison = converters.compareTwoNumber(item.min_rate, formattedRate) > 0;
           
           return item.user_address.toLowerCase() === this.props.account.address.toLowerCase() &&
-                item.status === constants.LIMIT_ORDER_CONFIG.status.OPEN &&
-                rateComparison;
+                item.status === constants.LIMIT_ORDER_CONFIG.status.OPEN && rateComparison;
         } 
         
         return false;
