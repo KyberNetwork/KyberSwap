259a4a3132325314916c840d6e6f4d88a19d4223
trungnx <trung@kyber.network>
2020-03-05 13:34:13 +0700

    Fix portfolio back button does not show when not imported

--- src/js/containers/Portfolio/PortfolioTab.js
+++ src/js/containers/Portfolio/PortfolioTab.js
@@ -1,4 +1,4 @@
-import React  from "react"
+import React, { Fragment } from "react"
 import { PORTFOLIO_TAB } from "../../services/constants";
 
 const PortfolioTab = (props) => {
@@ -9,22 +9,26 @@ const PortfolioTab = (props) => {
   return (
     <div className="portfolio__navigation theme__background-2">
       <div className="portfolio__navigation-arrow theme__arrow-icon" onClick={backFromPortfolio}/>
-      <div
-        className={`portfolio__navigation-item ${props.mobileTab === PORTFOLIO_TAB.overview ? 'portfolio__navigation-item--active' : ''}`}
-        onClick={() => props.switchMobileTab(PORTFOLIO_TAB.overview)}
-      >
-        Overview
-      </div>
-      <div
-        className={`portfolio__navigation-item ${props.mobileTab === PORTFOLIO_TAB.balance ? 'portfolio__navigation-item--active' : ''}`}
-        onClick={() => props.switchMobileTab(PORTFOLIO_TAB.balance)}>
-        Tokens
-      </div>
-      <div
-        className={`portfolio__navigation-item ${props.mobileTab === PORTFOLIO_TAB.history ? 'portfolio__navigation-item--active' : ''}`}
-        onClick={() => props.switchMobileTab(PORTFOLIO_TAB.history)}>
-        Tx History
-      </div>
+      {props.isImported && (
+        <Fragment>
+          <div
+            className={`portfolio__navigation-item ${props.mobileTab === PORTFOLIO_TAB.overview ? 'portfolio__navigation-item--active' : ''}`}
+            onClick={() => props.switchMobileTab(PORTFOLIO_TAB.overview)}
+          >
+            Overview
+          </div>
+          <div
+            className={`portfolio__navigation-item ${props.mobileTab === PORTFOLIO_TAB.balance ? 'portfolio__navigation-item--active' : ''}`}
+            onClick={() => props.switchMobileTab(PORTFOLIO_TAB.balance)}>
+            Tokens
+          </div>
+          <div
+            className={`portfolio__navigation-item ${props.mobileTab === PORTFOLIO_TAB.history ? 'portfolio__navigation-item--active' : ''}`}
+            onClick={() => props.switchMobileTab(PORTFOLIO_TAB.history)}>
+            Tx History
+          </div>
+        </Fragment>
+      )}
     </div>
   )
 };
