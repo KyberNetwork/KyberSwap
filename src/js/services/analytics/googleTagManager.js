966f464afaf8bd4a37b0eb8757dbd6c39dad1e4a
Tuan Nguyen <edwardevans094@gmail.com>
2020-03-05 15:49:27 +0700

    change google tag id

--- src/js/services/analytics/googleTagManager.js
+++ src/js/services/analytics/googleTagManager.js
@@ -11,13 +11,13 @@ export default class GoogleTagmanager {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WWFDZ4M')
+        })(window,document,'script','dataLayer','GTM-NFK79RR')
     } else {
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-WWFDZ4M')
+        })(window,document,'script','dataLayer','GTM-KMWZ7XB')
     }
 
     this.addUserIdentity()
