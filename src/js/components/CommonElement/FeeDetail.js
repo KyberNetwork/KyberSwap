d0ebc9bf4eb7b04a3175ff1b9407355c5aebd986
trungnx <trung@kyber.network>
2020-05-27 10:04:32 +0700

    Add warning for swap with high slippage

--- src/js/components/CommonElement/FeeDetail.js
+++ src/js/components/CommonElement/FeeDetail.js
@@ -5,7 +5,7 @@ const FeeDetail = (props) => {
   var totalGas = +calculateGasFee(props.gasPrice, props.gas)
   return (
     <div className="gas-configed theme__text-4">
-      <div className={"title-fee"}>{props.translate("transaction.transaction_fee") || 'Transaction Fee'}</div>
+      <div className={"title-fee theme__text-5"}>{props.translate("transaction.transaction_fee") || 'Transaction Fee'}</div>
       <div className={"total-fee"}>
         <span className={"total-fee__number theme__text"}>{totalGas.toString()} ETH</span>
         <span className={"total-fee__formula theme__text-6"}>{props.gasPrice} Gwei (Gas Price) * {props.gas} (Gas Limit)</span>
