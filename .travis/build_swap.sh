1133215efd0c2e607e5ba8a5ac84c27dac739025
Thang Dinh <vietthang207@gmail.com>
2020-06-22 11:17:20 +0700

    add auto-build for staging

--- .travis/build_swap.sh
+++ .travis/build_swap.sh
@@ -6,6 +6,8 @@ printf 'Building swap from commit %s' "$TRAVIS_COMMIT"
 if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
   npm run build_auto_dev
   npm run build_auto_ropsten
+elif [[ "$TRAVIS_BRANCH" == "staging" ]]; then
+  npm run build_auto_staging
 elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
   npm run build-ropsten
   npm run build-staging
