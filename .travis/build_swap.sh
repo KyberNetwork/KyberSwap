eead2b5ec23a3ccd75031721ae43e1a838c6f05c
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2020-07-03 09:52:03 +0700

    fix staging env

--- .travis/build_swap.sh
+++ .travis/build_swap.sh
@@ -7,7 +7,7 @@ if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
   npm run build_auto_dev
   npm run build_auto_ropsten
 elif [[ "$TRAVIS_BRANCH" == "staging" ]]; then
-  npm run build_auto_staging_limit_order
+  npm run build_auto_staging
 elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
   npm run build_auto_ropsten
   npm run build_auto_production
