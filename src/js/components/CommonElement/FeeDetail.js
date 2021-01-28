cc541f374c664b0e27cc82ee83ced49f82808fdd
trungnx <trungngoxuan92@gmail.com>
2021-01-28 15:49:22 +0700

    Fix gas total is empty in fee details

--- src/js/components/CommonElement/FeeDetail.js
+++ src/js/components/CommonElement/FeeDetail.js
@@ -1,7 +1,10 @@
 import React from "react"
 import ReactTooltip from "react-tooltip";
+import { calculateGasFee } from "../../utils/converter";
 
 const FeeDetail = (props) => {
+  const totalGas = props.totalGas ? props.totalGas : +calculateGasFee(props.gasPrice, props.gas);
+
   return (
     <div className="gas-configed theme__text-4">
       <div className={"title-fee theme__text-5"}>
@@ -12,7 +15,7 @@ const FeeDetail = (props) => {
         <ReactTooltip className="common__tooltip" place="top" id="max-gas-info" type="light"/>
       </div>
       <div className={"total-fee"}>
-        <span className={"total-fee__number theme__text"}>{props.totalGas.toString()} ETH</span>
+        <span className={"total-fee__number theme__text"}>{totalGas.toString()} ETH</span>
         <span className={"total-fee__formula theme__text-6"}>{props.gasPrice} Gwei (Gas Price) * {props.gas} (Gas Limit)</span>
       </div>
       {(props.reserveRoutingEnabled && props.reserveRoutingChecked) && (
